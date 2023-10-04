7afa73beba3af1b6d9a193c85427918d96c10b45
joshhayles <joshua.hayles@datadoghq.com>
2023-10-04 10:14:38 -0600

    flare matching config

---
+++ flare_type.js
@@ -0,0 +1,17 @@
+const statusLogPath = 'https://help.datadoghq.com/attachments/token/5bPyRk1nbAbWB1X9m4NLYhXYc/?name=datadog-agent-2023-08-16T22-21-37Z-info.zip'
+
+// Extract the last part of the URL
+const splitLogPath = statusLogPath.split('=')[1];
+
+// Define the REGEX pattern
+const regexPattern = /(.+)-(\w+)\.zip/;
+
+// Use RegExp.exec() to search for matches
+const match = regexPattern.exec(splitLogPath);
+
+if (match) {
+    const status = match[2].charAt(0).toUpperCase() + match[2].slice(1);
+    console.log(status);
+} else {
+    console.log("Unable to extract status information from the URL.");
+}
